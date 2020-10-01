ec735cc7ec70378484280de586e5586d4b729cfe
Samuel Aditia <samueladitia95@gmail.com>
2020-10-01 21:51:12 +0700

    1. Add news headline routes and controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/index')
 const { comparePass } = require('../helpers/bcrypt')
 const { signToken } = require('../helpers/jwt')
+const axios = require("axios");
 
 class UserController {
     static register(req, res, next) {
@@ -40,6 +41,56 @@ class UserController {
 			.catch((err) => {
 				next(err);
 			});
+    }
+
+    static async headline(req, res, next) {
+        try {
+            const results = []
+
+            // NEWS API
+            const data1 = await axios({
+                method: "get",
+                url: "https://newsapi.org/v2/top-headlines?country=id",
+                headers: {Authorization: process.env.NEWS_API}
+            })
+            results.push({
+                title: data1.data.articles[0].title,
+                description: data1.data.articles[0].description,
+                publishedAt: data1.data.articles[0].publishedAt.slice(0, 10),
+                news_url: data1.data.articles[0].url
+            })
+
+            //currents api
+            const data2 = await axios({
+                method: "get",
+                url: `https://api.currentsapi.services/v1/latest-news?apiKey=${process.env.CURRENTS_API}`
+            })
+            results.push({
+                title: data2.data.news[0].title,
+                description: data2.data.news[0].description,
+                publishedAt: data2.data.news[0].published.slice(0, 10),
+                news_url: data2.data.news[0].url
+            })
+
+            const data3 = await axios({
+                    method: "get",
+                    url: "https://newscatcher.p.rapidapi.com/v1/latest_headlines?lang=en&media=True",
+                    headers: {
+                        "x-rapidapi-host": "newscatcher.p.rapidapi.com",
+                        "x-rapidapi-key": process.env.NEWS_CATCHER,
+                        "useQueryString": true
+                    }
+                })
+            results.push({
+                title: data3.data.articles[0].title,
+                description: data3.data.articles[0].summary,
+                publishedAt: data3.data.articles[0].published_date.slice(0, 10),
+                news_url: data3.data.articles[0].url
+            })
+            res.status(200).json(results)
+        } catch(err) {
+            next(err)
+        }
     }
 }
 
